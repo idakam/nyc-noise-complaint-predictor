c061c7fea41556221ab6b08761899487950a2258
Ida <ida.kamali10@gmail.com>
2026-06-01 23:08:00 -0400

    trying agaiin

--- backend/deploy_to_hf.sh
+++ backend/deploy_to_hf.sh
@@ -9,6 +9,6 @@ hf upload idakam/nyc-noise-backend README.md README.md --repo-type space
 hf upload idakam/nyc-noise-backend routers/ routers/ --repo-type space
 hf upload idakam/nyc-noise-backend src/ src/ --repo-type space
 hf upload idakam/nyc-noise-backend models/ models/ --repo-type space
-# hf upload idakam/nyc-noise-backend data/processed/ data/processed/ --repo-type space
+hf upload idakam/nyc-noise-backend data/processed/ data/processed/ --repo-type space
 
 echo "✅ Done! Check https://huggingface.co/spaces/idakam/nyc-noise-backend"
